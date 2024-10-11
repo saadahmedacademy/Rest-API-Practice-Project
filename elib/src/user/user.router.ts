5fabbaf9a062b0fe80a1ed231b2841bc42f82f4b
Muhammad Saad Ahmed <147624130+saadahmedacademy@users.noreply.github.com>
2024-10-11 21:36:28 +1200

    Add login controller.

--- elib/src/user/user.router.ts
+++ elib/src/user/user.router.ts
@@ -1,9 +1,12 @@
 import express from 'express';
-import { registerUser } from './userControllers';
+import { loginUser, registerUser } from './userControllers';
 
 // create user routes
 const userRouter = express.Router();
 
+// To register a new user
 userRouter.route('/register').post(registerUser);
+// To login a user
+userRouter.route('/login').post(loginUser);
 
 export default userRouter;
--- elib/src/user/userControllers.ts
+++ elib/src/user/userControllers.ts
@@ -1,88 +1,111 @@
 import { NextFunction, Response, Request } from "express";
-import createHttpError from 'http-errors';
-import bcrypt from 'bcrypt';
+import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
 import asyncHandler from "../utils/asyncHandler";
 import { User } from "./user.model";
 import { ApiResponse } from "../utils/responseHandler";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { userSchema } from "./userTypes";
 
 const registerUser = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body;
-    console.log(req.body)
+    console.log(req.body);
 
     // Check that all fields are present
     if (!name || !email || !password) {
       return next(createHttpError(400, "All fields are required"));
     }
+    try {
+      // Check if user with the email already exists
+      const existedUser = await User.findOne({ email });
+      if (existedUser) {
+        return next(
+          createHttpError(409, "User with this email already exists")
+        );
+      }
 
-    // Check if user with the email already exists
-    const existedUser = await User.findOne({ email });
-    if (existedUser) {
-      return next(createHttpError(409, "User with this email already exists"));
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create a new user
+      const createUser: userSchema = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      if (!createUser) {
+        return next(
+          createHttpError(500, "Something went wrong while creating user")
+        );
+      }
+
+      // Generate JWT token
+      const token = sign(
+        { _id: createUser._id },
+        config.screte_key! as string,
+        { expiresIn: "7d", algorithm: "HS256" }
+      );
+
+      // Send response with the created user ID and token
+      return res.status(201).json(
+        ApiResponse(201, "User created successfully", {
+          accessToken: token,
+        })
+      );
+    } catch (error) {
+      return next(createHttpError(500, "Something went wrong whle registering user" + error));
     }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user
-    const createUser = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    if (!createUser) {
-      return next(createHttpError(500, "Something went wrong while creating user"));
-    }
-
-    // Generate JWT token
-    const token = sign(
-      { _id: createUser._id },
-      config.screte_key! as string, 
-      { expiresIn: '7d' , algorithm:"HS256"}
-    );
-
-    // Send response with the created user ID and token
-    return res.status(201).json(
-      ApiResponse(201, "User created successfully", {
-        "accessToken": token,
-      })
-    );
   }
 );
 
-const loginUser = asyncHandler(
-    async (req, res, next) => {
-      const { email, password, name } = req.body;
+const loginUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   
-      if (!password || (!email && !name)) {
-        return next(createHttpError(400, "Password is required along with either email or name"));
-      }
-  
-      // Find user by email or name
-      const user = await User.findOne({
-        $or: [{ email }, { name }],
-      });
-  
-      if (!user) {
-        return next(createHttpError(401, "Invalid email or password"));
-      }
-  
-      // Compare the provided password with the stored hashed password
-      const isPasswordValid =  bcrypt.compare(password, user.password);
-      if (!isPasswordValid) {
-        return next(createHttpError(401, "Invalid email or password"));
-      }
-  
-      // Send successful response with the user data
-      res.status(200).json(
-        ApiResponse(200, "User login successful", user)
-      );
+  const { email, password } = req.body;
+  if(!email || !password){
+    
+    return next(
+      createHttpError(
+        400,
+        "Email and password are required to login. Please provide email and password"
+      )
+    );
+  }
+  try {
+
+    // Check if user with the email already exists
+    const existedUser = await User.findOne({ email });
+    if (!existedUser) {
+      return next(createHttpError(40, "Invalid user email to login or user not found"));
     }
-  );
-  
 
+    // Check if password is correct
+    const isMatch = await bcrypt.compare(password, existedUser.password);
 
-export { registerUser ,loginUser};
+    if (!isMatch) {
+      return next(createHttpError(401, "Invalid user password to login or user not found"));
+    }
+     
+   
+      // Generate JWT token
+      const token = sign(
+        { _id: existedUser._id },
+        config.screte_key! as string,
+        { expiresIn: "7d", algorithm: "HS256" }
+      );
+
+      // Send response with the created user ID and token
+      return res.json(
+        ApiResponse(201,"User login successfully ",{
+          accessToken:token
+        })
+      );
+    
+  } catch (error) {
+    return next(createHttpError(500, "Something went wrong while login user" + error));
+  }
+});
+
+export { registerUser, loginUser };
