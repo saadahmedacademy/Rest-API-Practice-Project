c756e0947a5add01dd7e7bc3ff1519249e91238e
Muhammad Saad Ahmed <147624130+saadahmedacademy@users.noreply.github.com>
2024-11-24 12:42:35 +0000

    Set the server code

--- elib/src/server.ts
+++ elib/src/server.ts
@@ -1,22 +1,31 @@
-import app from './expressSetup';
-import { config } from './config/config';
 import connectDB from './db/db';
-
+import { config } from './config/config';
+import app from './expressSetup';
 const startServer = async () => {
   try {
-    // Connect to the database
-    await connectDB();
-    console.log('Database connected successfully');
 
+    await connectDB();
+
+    // Define the port, defaulting to 4000 if not specified in config
     const port = config.port || 4000;
 
-    // Start the server
-    app.listen(port, () => {
+    // Start the server on the specified port
+    const server = app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
+    });
+
+    // Handle server errors
+    server.on('error', (err: NodeJS.ErrnoException) => {
+      if (err.code === 'EADDRINUSE') {
+        console.error(`Port ${port} is already in use. Trying another port...`);
+        server.listen(0); // Listen on a random available port
+      } else {
+        throw err;
+      }
     });
   } catch (error) {
     console.error('Error starting the server:', error);
-    process.exit(1); // Exit with failure
+    process.exit(1); // Exit the process with a failure code
   }
 };
 
