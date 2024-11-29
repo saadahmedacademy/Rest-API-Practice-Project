dd83787d94fcdf426318639ad4403c6ede47f7b2
Muhammad Saad Ahmed <147624130+saadahmedacademy@users.noreply.github.com>
2024-11-29 19:27:53 +1200

    Update userControllers.ts

--- elib/src/user/userControllers.ts
+++ elib/src/user/userControllers.ts
@@ -1,112 +1,95 @@
-import { NextFunction, Response, Request } from "express";
 import createHttpError from "http-errors";
-import bcrypt from "bcrypt";
 import asyncHandler from "../utils/asyncHandler";
-import { User } from "./user.model";
-import { ApiResponse } from "../utils/responseHandler";
-import { sign } from "jsonwebtoken";
-import { config } from "../config/config";
-import { userSchema } from "./userTypes";
+import { NextFunction, Response, Request } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "node:path";
+import fs from "node:fs/promises";
+import { Book } from "./book.model";
 
-const registerUser = asyncHandler(
+const createBook = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { name, email, password } = req.body;
-    console.log(req.body);
+    const { title, genre } = req.body; 
 
-    // Check that all fields are present
-    if (!name || !email || !password) {
-      return next(createHttpError(400, "All fields are required"));
+
+    const user = req.user
+    console.log(user)
+
+  
+
+
+    const files = req.files as { [key: string]: Express.Multer.File[] };
+
+    // Validate required files
+    if (!files?.coverImage || !files?.bookFile) {
+      return next(
+        createHttpError(400, "Both 'coverImage' and 'bookFile' are required.")
+      );
     }
+
+    const coverImage = files.coverImage[0];
+    const bookFile = files.bookFile[0];
+
+    const coverImagePath = path.resolve(
+      __dirname,
+      "../../public/tempFiles",
+      coverImage.filename
+    );
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../public/tempFiles",
+      bookFile.filename
+    );
+
     try {
-      // Check if user with the email already exists
-      const existedUser = await User.findOne({ email });
-      if (existedUser) {
-        return next(
-          createHttpError(409, "User with this email already exists")
-        );
-      }
+      // Parallel upload to Cloudinary
+      const [uploadCoverImage, uploadBookFile] = await Promise.all([
+        cloudinary.uploader.upload(coverImagePath, { folder: "book-cover" }),
+        cloudinary.uploader.upload(bookFilePath, {
+          resource_type: "raw",
+          folder: "book-pdfs",
+          format: "pdf",
+        }),
+      ]);
 
-      // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      // Create a new user
-      const createUser: userSchema = await User.create({
-        name,
-        email,
-        password: hashedPassword,
+      // To upload the Cloudinary URLs into the database
+      const newBook = await Book.create({
+        title,
+        genre,
+        author: "66f960978e49d42a4bacd8a4", // Example author
+        coverImage: uploadCoverImage.secure_url,
+        bookFile: uploadBookFile.secure_url,
       });
 
-      if (!createUser) {
+      console.log("New Book Created:", newBook);
+
+      if (!newBook) {
         return next(
-          createHttpError(500, "Something went wrong while creating user")
+          createHttpError(
+            400,
+            "Error while creating newBook model in the database"
+          )
         );
       }
 
-      // Generate JWT token
-      const token = sign(
-        { _id: createUser._id },
-        config.screte_key! as string,
-        { expiresIn: "7d", algorithm: "HS256" }
-      );
-
-      // Send response with the created user ID and token
-      return res.status(201).json(
-        ApiResponse(201, "User created successfully", {
-          accessToken: token,
-        })
-      );
+      // Send response after successful creation
+      return res.status(201).json({
+        message: "New book model created successfully",
+        coverImageUrl: uploadCoverImage.secure_url,
+        bookFileUrl: uploadBookFile.secure_url,
+      });
     } catch (error) {
-      return next(createHttpError(500, "Something went wrong whle registering user" + error));
+      
+      return res
+        .status(500)
+        .json({ message: "error white creating book", error });
+    } finally {
+      // Cleanup: Remove temporary files
+      await Promise.allSettled([
+        fs.unlink(coverImagePath),
+        fs.unlink(bookFilePath),
+      ]);
     }
   }
 );
 
-const loginUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  
-  const { email, password } = req.body;
-  if(!email || !password){
-    
-    return next(
-      createHttpError(
-        400,
-        "Email and password are required to login. Please provide email and password"
-      )
-    );
-  }
-  try {
-
-    // Check if user with the email already exists
-    const existedUser = await User.findOne({ email });
-    
-    if (!existedUser) {
-      return next(createHttpError(40, "Invalid user email to login or user not found"));
-    }
-
-    // Check if password is correct
-    const isMatch = await bcrypt.compare(password, existedUser.password);
-
-    if (!isMatch) {
-      return next(createHttpError(401, "Invalid user password to login or user not found"));
-    }
-     
-   
-      // Generate JWT token
-      const token = sign(
-        { _id: existedUser._id },
-        config.screte_key! as string,
-        { expiresIn: "7d", algorithm: "HS256" }
-      );
-
-      // Send response with the created user ID and token
-      return res.json(
-        ApiResponse(201,"User login successfully ",{
-          accessToken:token
-        })
-      );
-    
-  } catch (error) {
-    return next(createHttpError(500, "Something went wrong while login user" + error));
-  }
-});
-
-export { registerUser, loginUser };
+export { createBook };
